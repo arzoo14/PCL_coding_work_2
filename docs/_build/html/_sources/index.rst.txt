cec5f0abf1d98945b33e6af5f0b1b2b0038a135f
arzoo14 <arzoopprajapati11@gmail.com>
2020-04-10 12:06:14 +0530

    changes

--- docs/_build/html/_sources/index.rst.txt
+++ docs/_build/html/_sources/index.rst.txt
@@ -13,15 +13,16 @@ The Point Cloud Library (PCL) is a standalone, large scale, open project for 2D/
 
 PCL is released under the terms of the BSD license, and thus free for commercial and research use. We are financially supported by a consortium of commercial companies, with our own non-profit organization, Open Perception. We would also like to thank individual donors and contributors that have been helping the project.
 
-Contents:
+
+   
 
 .. toctree::
+   :caption: Basic Tutorials
    :maxdepth: 2
-
+ 
    adding custom ptype
    benchmark
    building pcl
-   test
 
 
 
--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+import sphinx_rtd_theme
+
+extensions = [
+    "sphinx_rtd_theme"
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -46,7 +51,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'PCL documentation'
+project = 'PCL DOCUMENTATION'
 copyright = '2020, Arzoo'
 author = 'Arzoo'
 
@@ -83,13 +88,28 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-XXXXXXX-1',  
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': 'dark blue',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': -1,
+    'includehidden': False,
+    'titles_only': False
+}
+#html_logo = "image/pcl.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
